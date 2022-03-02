575777e36d0a005718928056495172d193d9c98e
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-03-02 23:36:40 +0300

    add missing value algorithm

---
+++ missingValue.js
@@ -0,0 +1,41 @@
+/*
+ story
+ An array A consisting of N different integers is given.
+  The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+    Your goal is to find that missing element.
+
+    Write a function:
+
+    function solution(A);
+
+    that, given an array A, returns the value of the missing element.
+
+    For example, given array A such that:
+
+    A[0] = 2
+    A[1] = 3
+    A[2] = 1
+    A[3] = 5
+    the function should return 4, as it is the missing element.
+
+*/
+
+const missingElem = (A) => {
+  const missingVals = []
+  const sortedArray = A.sort((a, b) => a - b)
+  for (let i = 1; i <= sortedArray[sortedArray.length - 1]; i++) {
+    if (A.indexOf(i) === -1) {
+      missingVals.push(i)
+    }
+  }
+
+  if (missingVals.length > 1) {
+    return missingVals
+  }
+  return missingVals[0]
+}
+
+let array1 = [5, 1, 3, 2, 12]
+
+console.log(missingElem(array1))
